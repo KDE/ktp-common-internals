7fa5697976e387141db3f1e6bb18661b7f915d86
George Goldberg <grundleborg@googlemail.com>
2009-01-26 18:05:19 +0000

    Add a stub model for the accounts list.
    svn path=/trunk/playground/network/telepathy-accounts-kcm/; revision=917028

---
+++ KTp/Models/accounts-list-model.cpp
@@ -0,0 +1,36 @@
+/*
+ * This file is part of telepathy-accounts-kcm
+ *
+ * Copyright (C) 2009 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "accounts-list-model.h"
+
+AccountsListModel::AccountsListModel(QObject *parent)
+ : QAbstractListModel(parent)
+{
+    // TODO: Implement me!
+}
+
+AccountsListModel::~AccountsListModel()
+{
+    // TODO: Implement me!
+}
+
+
+#include "accounts-list-model.moc"
+
---
+++ KTp/Models/accounts-list-model.h
@@ -0,0 +1,38 @@
+/*
+ * This file is part of telepathy-accounts-kcm
+ *
+ * Copyright (C) 2009 Collabora Ltd. <http://www.collabora.co.uk/>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef TELEPATHY_ACCOUNTS_KCM_ACCOUNTS_LIST_MODEL_H
+#define TELEPATHY_ACCOUNTS_KCM_ACCOUNTS_LIST_MODEL_H
+
+#include <QAbstractListModel>
+
+class AccountsListModel : public QAbstractListModel
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(AccountsListModel);
+
+public:
+    explicit AccountsListModel(QObject *parent = 0);
+    virtual ~AccountsListModel();
+
+};
+
+#endif // header guard
+
