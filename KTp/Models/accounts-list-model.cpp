aa8e0daf13079b099ad1600d26e57696b23803fc
George Goldberg <grundleborg@googlemail.com>
2009-02-14 21:28:32 +0000

    When an account becomes ready, we should add it to the accessible part of the model.
    svn path=/trunk/playground/network/telepathy-accounts-kcm/; revision=926228

--- KTp/Models/accounts-list-model.cpp
+++ KTp/Models/accounts-list-model.cpp
@@ -104,7 +104,18 @@ void AccountsListModel::addAccount(Telepathy::Client::Account *account)
 
 void AccountsListModel::onAccountItemReady()
 {
-    // TODO: Implement me!
+    AccountItem *item = qobject_cast<AccountItem*>(sender());
+    Q_ASSERT(item);
+    Q_ASSERT(m_unreadyAccounts.contains(item));
+    Q_ASSERT(!m_readyAccounts.contains(item));
+
+    beginInsertRows(QModelIndex(), m_readyAccounts.size(), m_readyAccounts.size()+1);
+    m_readyAccounts.append(item);
+    Q_ASSERT(1==m_unreadyAccounts.removeAll(item));
+    endInsertRows();
+
+    Q_ASSERT(!m_unreadyAccounts.contains(item));
+    Q_ASSERT(m_readyAccounts.contains(item));
 }
 
 void AccountsListModel::onAccountItemRemoved()
