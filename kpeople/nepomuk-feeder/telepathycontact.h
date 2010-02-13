1eeac42ea21722b54fd5204775436af61321c245
George Goldberg <grundleborg@googlemail.com>
2010-02-13 16:35:15 +0000

    Add a skeleton class for TelepathyContact.
    svn path=/trunk/playground/network/telepathy-integration-daemon/; revision=1089646

---
+++ kpeople/nepomuk-feeder/telepathycontact.h
@@ -0,0 +1,48 @@
+/*
+ * This file is part of telepathy-integration-daemon
+ *
+ * Copyright (C) 2010 Collabora Ltd. <info@collabora.co.uk>
+ *   @author George Goldberg <george.goldberg@collabora.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef TELEPATHY_INTEGRATION_DAEMON_TELEPATHYCONTACT_H
+#define TELEPATHY_INTEGRATION_DAEMON_TELEPATHYCONTACT_H
+
+#include <QtCore/QObject>
+
+class TelepathyAccount;
+
+/**
+ * This class takes care of one contact in the roster of a given telepathy account on the AM.
+ */
+class TelepathyContact : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit TelepathyContact(TelepathyAccount *parent = 0);
+    ~TelepathyContact();
+
+private:
+    Q_DISABLE_COPY(TelepathyContact);
+
+    TelepathyAccount *m_parent;
+};
+
+
+#endif // Header guard
+
