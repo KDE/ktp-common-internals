fa9660b33e81c81bc60154f04d87431b697f1800
David Edmundson <kde@davidedmundson.co.uk>
2011-01-08 23:54:46 +0000

    improved work on item delegate

--- KTp/Models/accounts-list-model.h
+++ KTp/Models/accounts-list-model.h
@@ -32,13 +32,13 @@ class AccountsListModel : public QAbstractListModel
     Q_OBJECT
     Q_DISABLE_COPY(AccountsListModel);
 
+public:
     enum Roles {
         ConnectionStateDisplayRole = Qt::UserRole+1,
         ConnectionStateIconRole = Qt::UserRole+2,
         ConnectionErrorMessageDisplayRole = Qt::UserRole+3
     };
 
-public:
     explicit AccountsListModel(QObject *parent = 0);
     virtual ~AccountsListModel();
     virtual int rowCount(const QModelIndex &index) const;
