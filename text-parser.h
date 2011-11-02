9f89c21c9455f104e41b393ef8b7955c0790f07c
Martin Klapetek <martin.klapetek@gmail.com>
2011-11-02 14:03:56 +0100

    Fix compile issues

--- text-parser.h
+++ text-parser.h
@@ -83,11 +83,6 @@ private:
      * @var TextParser
      */
     static TextParser *s_instance;
-
-    /**
-     * @var QRegExp
-     */
-    static QRegExp s_urlPattern(QString("\\b((?:(?:([a-z][\\w\\.-]+:/{1,3})|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|\\}\\]|[^\\s`!()\\[\\]{};:'\".,<>?%1%2%3%4%5%6])|[a-z0-9.\\-+_]+@[a-z0-9.\\-]+[.][a-z]{1,5}[^\\s/`!()\\[\\]{};:'\".,<>?%1%2%3%4%5%6]))").arg(QChar(0x00AB)).arg(QChar(0x00BB)).arg(QChar(0x201C)).arg(QChar(0x201D)).arg(QChar(0x2018)).arg(QChar(0x2019)));
 };
 
 #endif // TEXT_PARSER_H
