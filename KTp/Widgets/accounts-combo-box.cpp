e5b56e8e21f784d15d88e4f47e6e34040ae6cf1a
David Edmundson <kde@davidedmundson.co.uk>
2012-10-03 00:01:59 +0100

    Add a widget to show a combo of all accounts

---
+++ KTp/Widgets/accounts-combo-box.cpp
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2012 David Edmundson <kde@davidedmundson.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+#include "accounts-combo-box.h"
+
+#include <TelepathyQt/AccountManager>
+#include <TelepathyQt/Account>
+
+#include <KTp/Models/accounts-list-model.h>
+
+
+class KTp::AccountsComboBox::Private
+{
+public:
+    AccountsListModel *model;
+};
+
+KTp::AccountsComboBox::AccountsComboBox(QWidget *parent) :
+    QComboBox(parent),
+    d(new KTp::AccountsComboBox::Private())
+{
+    d->model = new AccountsListModel(this);
+    setModel(d->model);
+}
+
+void KTp::AccountsComboBox::setAccountManager(const Tp::AccountManager &accountManager)
+{
+    //    d->model->settAccountManager(accountManager);
+}
+
+Tp::AccountPtr KTp::AccountsComboBox::currentAccount()
+{
+    return itemData(currentIndex(), AccountsListModel::AccountRole).value<Tp::AccountPtr>();
+}
+
+
+
+
---
+++ KTp/Widgets/accounts-combo-box.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2012 David Edmundson <kde@davidedmundson.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+#ifndef ACCOUNTSCOMBOBOX_H
+#define ACCOUNTSCOMBOBOX_H
+
+#include <QComboBox>
+#include <TelepathyQt/Types>
+
+namespace KTp {
+
+class AccountsComboBox : public QComboBox
+{
+    Q_OBJECT
+    Q_DISABLE_COPY(AccountsComboBox)
+public:
+    explicit AccountsComboBox(QWidget *parent = 0);
+
+public:
+    void setAccountManager(const Tp::AccountManager &accountManager);
+    Tp::AccountPtr currentAccount();
+
+private:
+    class Private;
+    Private * const d;
+};
+}
+
+#endif // ACCOUNTSCOMBOBOX_H
