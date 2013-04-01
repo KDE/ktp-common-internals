6cbefb04441bab0d25dde78d95a2adee4d163fd5
David Edmundson <kde@davidedmundson.co.uk>
2013-04-01 18:02:26 +0100

    Prevent crashes with debug kdelibs
    Stop printing the library name as the function. Debug releases of kdelibs try to
    parse this and assert as "Telepathy-qt 0.9.3" is not a valid function name
    
    Reviewed-by: Aleix Pol

--- KTp/debug.cpp
+++ KTp/debug.cpp
@@ -37,7 +37,7 @@ static void tpDebugCallback(const QString &libraryName,
     if (Q_UNLIKELY(libraryString.isEmpty())) {
         libraryString = QString::fromLatin1("%1 %2").arg(libraryName, libraryVersion);
     }
-    kDebugStream(type, s_tpqtDebugArea, __FILE__, __LINE__, libraryString.toLatin1()) << qPrintable(msg);
+    kDebugStream(type, s_tpqtDebugArea, __FILE__, __LINE__, 0) << qPrintable(msg);
 }
 } // namespace
 } // namespace KTp
