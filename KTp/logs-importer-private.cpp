fb28de93e26b6c24a9667a16b21259e1737b3d25
Dan Vrátil <dvratil@redhat.com>
2013-08-22 14:43:05 +0200

    Don't delete LogsImporter thread while it's still running
    When LogsImporter is deleted, it just deletes the importer thread
    even when it's running, which will obviously crash. This patch adds
    a safety guard (calling QThread::wait() before deleting it) and a
    method to stop the thread prematurely so that we don't wait() for
    the entire import to finish.
    
    REVIEW: 112188

--- KTp/logs-importer-private.cpp
+++ KTp/logs-importer-private.cpp
@@ -31,13 +31,18 @@ LogsImporter::Private::Private(KTp::LogsImporter* parent)
   , m_month(0)
   , m_year(0)
   , m_isMUCLog(false)
+  , m_shouldStop(0)
 {
 
 }
 
 LogsImporter::Private::~Private()
 {
+}
 
+void LogsImporter::Private::stop()
+{
+    m_shouldStop = 1;
 }
 
 void LogsImporter::Private::setAccountId(const QString& accountId)
@@ -55,6 +60,10 @@ void LogsImporter::Private::run()
 
     Q_FOREACH (const QString &file, files) {
         convertKopeteLog(file);
+
+        if (m_shouldStop == 1) {
+            return;
+        }
     }
 }
 
@@ -362,11 +371,16 @@ void LogsImporter::Private::convertKopeteLog(const QString& filepath)
     }
 
     for (int i = 0; i < kopeteMessages.count(); i++) {
-        QDomElement kopeteMessage = kopeteMessages.item(i).toElement();
+        const QDomElement kopeteMessage = kopeteMessages.item(i).toElement();
+        const QDomElement ktpMessage = convertKopeteMessage(kopeteMessage);
 
-        QDomElement ktpMessage = convertKopeteMessage(kopeteMessage);
+        if (!ktpMessage.isNull()) {
+            m_ktpLogElement.appendChild(ktpMessage);
+        }
 
-        m_ktpLogElement.appendChild(ktpMessage);
+        if (m_shouldStop == 1) {
+            return;
+        }
     }
 
     saveKTpDocument();
