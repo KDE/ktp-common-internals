c98c7b6c80ca7bdcc0dc54bf8a20b5cc2747eb66
l10n daemon script <scripty@kde.org>
2013-08-29 03:55:39 +0000

    SVN_SILENT made messages (.desktop file)

--- KTp/Logger/plugins/tplogger/ktploggerplugin_tplogger.desktop.cmake
+++ KTp/Logger/plugins/tplogger/ktploggerplugin_tplogger.desktop.cmake
@@ -1,11 +1,13 @@
 [Desktop Entry]
 Name=Telepathy Logger plugin
+Name[fi]=Telepathy Logger -liitännäinen
 Name[nl]=Telepathy Logger-plug-in
 Name[sl]=Vstavek za beleženje za Telepathy
 Name[sv]=Telepathy-loggningsinsticksprogram
 Name[uk]=Додаток ведення журналу Telepathy
 Name[x-test]=xxTelepathy Logger pluginxx
 Comment=Provides integration with Telepathy Logger
+Comment[fi]=Tarjoaa integraation Telepathy Loggerille
 Comment[nl]=Biedt integratie met Telepathy Logger
 Comment[sl]=Omogoča podpora za beleženje za Telepathy
 Comment[sv]=Tillhandahåller integrering med Telepathy-loggningsinsticksprogram
