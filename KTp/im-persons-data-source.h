d7db8d4204d13c05ee3e4d2db759557a09591099
Martin Klapetek <mklapetek@kde.org>
2013-11-14 19:44:57 +0100

    Port the KTp data source to the latest KPeople API changes

--- KTp/im-persons-data-source.h
+++ KTp/im-persons-data-source.h
@@ -24,39 +24,15 @@
 
 #include <TelepathyQt/Types>
 
-#include "KTp/contact.h"
 #include "KTp/ktp-export.h"
-
-#include <KABC/Addressee>
-
-namespace KTp { class GlobalContactManager; }
-namespace Tp { class PendingOperation; }
 
 class KTP_EXPORT IMPersonsDataSource : public KPeople::BasePersonsDataSource
 {
-    Q_OBJECT
 public:
     IMPersonsDataSource(QObject *parent, const QVariantList &data);
     virtual ~IMPersonsDataSource();
 
-    virtual const KABC::Addressee contact(const QString &contactId);
-    virtual const KABC::Addressee::Map allContacts();
-
-    KTp::ContactPtr contactForContactId(const QString &contactId) const;
-    Tp::AccountPtr accountForContact(const KTp::ContactPtr &contact) const;
-
-    Tp::AccountManagerPtr accountManager() const;
-
-private Q_SLOTS:
-    void onAccountManagerReady(Tp::PendingOperation *op);
-    void onContactChanged();
-    void onContactInvalidated();
-
-private:
-    Q_PRIVATE_SLOT(d, void onAllKnownContactsChanged(const Tp::Contacts &contactsAdded, const Tp::Contacts &contactsRemoved) )
-
-    class Private;
-    Private* d;
+    virtual AllContactsMonitor* createAllContactsMonitor();
 };
 
 #endif // IM_PERSONS_DATA_SOURCE_H
