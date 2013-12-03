5b8f53fae1413105441433e4016f0a794405496e
Martin Klapetek <mklapetek@kde.org>
2013-12-03 21:35:29 +0100

    Remove nepomuk feeder
    Not needed anymore

--- kpeople/nepomuk-feeder/cmake/modules/FindKTelepathyTestLib.cmake
+++
@@ -1,8 +0,0 @@
-# Try to find Telepathy-Qt4 Test Library
-#
-# Copyright (c) 2010, Dario Freddi <drf@kde.org>
-#
-# Redistribution and use is allowed according to the terms of the BSD license.
-# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-
-find_package( KTelepathyTestLib QUIET NO_MODULE PATHS ${LIB_INSTALL_DIR}/KTelepathy/TestLib/cmake )
