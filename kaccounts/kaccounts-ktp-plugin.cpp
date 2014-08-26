06598ea299d5c2bbe6013844d36670514a7ffb01
Martin Klapetek <mklapetek@kde.org>
2014-08-26 13:55:34 +0200

    Add KAccounts integration plugin
    Plugin to integrate with KAccounts' kded module
    
    REVIEW: 119931

---
+++ kaccounts/kaccounts-ktp-plugin.cpp
@@ -0,0 +1,97 @@
+/*
+    Copyright (C) 2014  Martin Klapetek <mklapetek@kde.org>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "kaccounts-ktp-plugin.h"
+
+#include <TelepathyQt/AccountManager>
+
+#include <KSharedConfig>
+#include <KConfigGroup>
+
+#include <KTp/Logger/log-manager.h>
+
+class KAccountsKTpPlugin::Private {
+public:
+    Tp::AccountManagerPtr accountManager;
+};
+
+KAccountsKTpPlugin::KAccountsKTpPlugin(QObject *parent)
+    : KAccountsDPlugin(parent),
+      d(new Private)
+{
+    Tp::registerTypes();
+
+    // Start setting up the Telepathy AccountManager.
+    Tp::AccountFactoryPtr  accountFactory = Tp::AccountFactory::create(QDBusConnection::sessionBus(),
+                                                                       Tp::Features() << Tp::Account::FeatureCore);
+
+    d->accountManager = Tp::AccountManager::create(accountFactory);
+    d->accountManager->becomeReady();
+}
+
+KAccountsKTpPlugin::~KAccountsKTpPlugin()
+{
+
+}
+
+void KAccountsKTpPlugin::onAccountCreated(const Accounts::AccountId accountId, const Accounts::ServiceList &serviceList)
+{
+    // TODO:
+    // Here will go account services setting up, eg. if one adds Google account
+    // in KAccounts, we need to check if he requested "Chat" service here and
+    // set up the account here
+}
+
+void KAccountsKTpPlugin::onAccountRemoved(const Accounts::AccountId accountId)
+{
+    // Lookup the config file and then proceed to remove the Tp account
+    // that corresponds with the account id
+    KSharedConfigPtr kaccountsConfig = KSharedConfig::openConfig(QStringLiteral("kaccounts-ktprc"));
+    KConfigGroup ktpKaccountsGroup = kaccountsConfig->group(QStringLiteral("kaccounts-ktp"));
+    QString accountUid = ktpKaccountsGroup.readEntry(QString::number(accountId));
+
+    if (accountUid.isEmpty()) {
+        qWarning() << "The config file returned emtpy account uid, aborting";
+        return;
+    }
+
+    Tp::AccountPtr account = d->accountManager->accountForObjectPath(accountUid);
+    if (account.isNull()) {
+        qWarning() << "Account manager returned invalid account, aborting";
+        return;
+    }
+
+    // FIXME keep this non-optional? The problem is that we can't show the "are you sure"
+    //       dialog here as that's too late at this point
+    if (1) {
+        KTp::LogManager *logManager = KTp::LogManager::instance();
+        logManager->clearAccountLogs(account);
+    }
+    account->remove();
+}
+
+void KAccountsKTpPlugin::onServiceEnabled(const Accounts::AccountId accountId, const Accounts::Service& service)
+{
+
+}
+
+void KAccountsKTpPlugin::onServiceDisabled(const Accounts::AccountId accountId, const Accounts::Service& service)
+{
+
+}
+
