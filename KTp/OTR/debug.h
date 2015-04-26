4c13926eb7d83ed01cb53598c25d0063d66d247c
Jonathan Riddell <jr@jriddell.org>
2015-04-26 11:42:42 +0200

    Fix e-mail address CCMAIL:aleixpol@kde.org

--- KTp/OTR/debug.h
+++ KTp/OTR/debug.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2014 Martin Klapetek <mklapetek@kde.org>
- * Copyright (C) 2014 Aleix Pol Gonzalez <aleixpol@blue-systems.com>
+ * Copyright (C) 2014 Aleix Pol Gonzalez <aleixpol@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
