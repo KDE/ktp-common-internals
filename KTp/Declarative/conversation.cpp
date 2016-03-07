59fe385a4e02cf479d1546aadd66f5e8317d3fd6
Martin Klapetek <mklapetek@kde.org>
2016-03-07 18:33:48 -0500

    [declarative] Return Tp::Account* rather than Tp::AccountPtr as the Q_PROPERTY
    Returning the pointer itself allows to use the object from QML directly,
    there's not much use to return the Tp::AccountPtr as Q_PROPERTY

--- KTp/Declarative/conversation.cpp
+++ KTp/Declarative/conversation.cpp
@@ -177,6 +177,15 @@ Tp::AccountPtr Conversation::account() const
     return d->account;
 }
 
+Tp::Account* Conversation::accountObject() const
+{
+    if (!d->account.isNull()) {
+        return d->account.data();
+    }
+
+    return 0;
+}
+
 void Conversation::setAccount(const Tp::AccountPtr &account)
 {
     d->account = account;
