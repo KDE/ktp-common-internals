294e1b91813ae6862efb834a884d41cc11e6d95e
Martin Klapetek <mklapetek@kde.org>
2016-03-09 17:10:33 -0500

    [declarative] Guard for empty channel
    Reviewed-by: Alexandr Akulich <akulichalexander@gmail.com>

--- KTp/Declarative/messages-model.cpp
+++ KTp/Declarative/messages-model.cpp
@@ -364,6 +364,10 @@ int MessagesModel::unreadCount() const
 
 void MessagesModel::acknowledgeAllMessages()
 {
+    if (d->textChannel.isNull()) {
+        return;
+    }
+
     QList<Tp::ReceivedMessage> queue = d->textChannel->messageQueue();
 
     d->textChannel->acknowledge(queue);
