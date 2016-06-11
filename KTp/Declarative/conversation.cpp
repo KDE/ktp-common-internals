d58fd6386a4cfc19e14b371c4b0af1a7acf79a19
Martin Klapetek <mklapetek@kde.org>
2016-06-11 10:25:11 -0400

    Add missing copyright
    Reviewed-by: Alexandr Akulich <akulichalexander@gmail.com>

--- KTp/Declarative/conversation.cpp
+++ KTp/Declarative/conversation.cpp
@@ -1,5 +1,6 @@
 /*
     Copyright (C) 2011  Lasath Fernando <kde@lasath.org>
+    Copyright (C) 2016  Martin Klapetek <mklapetek@kde.org>
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Lesser General Public
